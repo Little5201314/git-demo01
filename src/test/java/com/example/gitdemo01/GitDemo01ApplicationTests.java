51a087e7b04b377da4350cac956684530890f479
chenbo <chenbo1115father@163.com>
2022-04-23 23:22:14 +0800

    first commit

---
+++ src/test/java/com/example/gitdemo01/GitDemo01ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.gitdemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello git");
+    }
+
+}
